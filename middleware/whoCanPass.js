96910c2139dfc4786f17907c804e1ee68c087b35
enotowitch <en.enotowitch@gmail.com>
2024-01-18 13:01:52 +0200

    M: whoCanPass

--- middleware/whoCanPass.js
+++ middleware/whoCanPass.js
@@ -3,7 +3,7 @@ import { find, verifyToken } from "../controllers/functions.js"
 export const whoCanPass = async ({ req, res, next, role }) => {
 
     const token = req.headers.authorization
-    // if (!token) return ???
+    if (!token) return // ???
 
     const userId = await verifyToken(token)
     const foundUser = await find({ col: "users", query: { _id: userId } })

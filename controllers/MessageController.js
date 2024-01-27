0678b9376ca082064764f31372f6728cc47c60d7
enotowitch <en.enotowitch@gmail.com>
2024-01-27 14:11:11 +0200

    !!F: getRooms: in each Room USER sees: coach img, coach name; COACH sees: user img, user name

--- controllers/MessageController.js
+++ controllers/MessageController.js
@@ -26,13 +26,39 @@ export const getRooms = async (req, res) => { // room = coaching (DB model)
     // foundInfo must = foundEmails.length => allow 1 coach to have many courses
     let foundInfo = []
     for (let i = 0; i < foundEmails.length; i++) {
-        foundInfo.push(await find({ col: "users", query: { email: foundEmails[i] }, filter: { name: 1, img: 1, _id: 0 } }))
+        foundInfo.push(await find({ col: "users", query: { email: foundEmails[i] }, filter: { name: 1, img: 1, _id: 1 } }))
     }
     let allMessages = await find({ col: "messages", query: { room: { $in: foundRoomTokens } }, filter: { msg: 1, room: 1, updatedAt: 1, email: 1, isRead: 1, img: 1, _id: 0 } })
     const allMessagesReversed = [...allMessages].reverse()
     const lastMsgsArr = foundRoomTokens.map(token => allMessagesReversed.find(message => token === message.room && { msg: message.msg, room: message.room, createdAt: message.updatedAt, img: message.img }))
     const notReadMsgsArr = foundRoomTokens.map(token => allMessages.map(message => email !== message.email && message.isRead === false && token === message.room).filter(leaveOnlyTrue => leaveOnlyTrue))
-    const foundInfoWithRoomToken = foundInfo.map((foundInfo, ind) => ({ ...foundInfo, room: found?.[ind]?.room, msg: lastMsgsArr?.[ind]?.msg, msgImg: lastMsgsArr?.[ind]?.img, createdAt: lastMsgsArr?.[ind]?.updatedAt, notReadNum: notReadMsgsArr?.[ind]?.length, courseName: found?.[ind]?.courseName }))
+
+    const foundInfoWithRoomToken = foundInfo.map((info, infoInd) => ({
+        name: info?.[0]?.name,
+        img: info?.[0]?.img,
+        userId: info?.[0]?._id,
+        room: found?.[infoInd]?.room,
+        msg: lastMsgsArr?.[infoInd]?.msg,
+        msgImg: lastMsgsArr?.[infoInd]?.img,
+        createdAt: lastMsgsArr?.[infoInd]?.updatedAt,
+        notReadNum: notReadMsgsArr?.[infoInd]?.length,
+        isRead: lastMsgsArr?.[infoInd]?.isRead,
+        courseName: found?.[infoInd]?.courseName
+    }));
+
+    // ! rewrite for user: in each Room user sees: coach img, coach name
+    // coach sees: user img, user name
+    if (req.user.role === "user") {
+        const foundCoachInfoToDisplayInEachUserRoom = []
+        for (let i = 0; i < found.length; i++) {
+            foundCoachInfoToDisplayInEachUserRoom.push(await find({ col: "courses", query: { courseName: found[i]?.courseName }, filter: { coachName: 1, img: 1, _id: 0 } }))
+        }
+
+        foundInfoWithRoomToken.forEach((info, infoInd) => {
+            info.name = foundCoachInfoToDisplayInEachUserRoom?.[infoInd]?.[0]?.coachName
+            info.img = foundCoachInfoToDisplayInEachUserRoom?.[infoInd]?.[0]?.img
+        })
+    }
 
     // clear mongo info
     const roomsInfo = foundInfoWithRoomToken.map(found => {

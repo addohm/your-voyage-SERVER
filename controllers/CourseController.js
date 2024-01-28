4cc51c396627d6315f36ca6931137636d5b84087
enotowitch <en.enotowitch@gmail.com>
2024-01-28 10:37:10 +0200

    F: addCourse: update img

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -4,13 +4,12 @@ export const addCourse = async (req, res) => {
 
     const { coachEmail, _id } = req.body
     const foundCourse = await find({ col: "courses", query: { _id } })
+    const foundUser = await find({ col: "users", query: { email: coachEmail } })
+    const img = foundUser?.[0]?.img
 
     if (!foundCourse?.[0]) { // no course found => create course
-        const foundUser = await find({ col: "users", query: { email: coachEmail } })
-        const img = foundUser?.[0]?.img
-
         await create({ createObj: { ...req.body, img }, col: req.body.type })
     } else { // has course => update
-        await update({ update: { ...req.body }, col: req.body.type, filter: { _id } })
+        await update({ update: { ...req.body, img }, col: req.body.type, filter: { _id } })
     }
 }

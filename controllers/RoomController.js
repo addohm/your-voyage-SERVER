6d1321046b28e8c351590d1f92c9d5b8860f2f85
enotowitch <en.enotowitch@gmail.com>
2024-01-31 11:19:22 +0200

    !M: processRooms: +mutateRoomInfo

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -1,10 +1,11 @@
 import { find } from "./functions.js"
 
-// ! getRooms
+// ! getRooms (getRoomsCourses)
 export const getRooms = async (req, res) => { // room = coaching (DB model)
 
     const { email } = req.user // email = userEmail
 
+    // ! found
     let found
     if (req.user.role === "admin" || req.user.role === "coach") {
         // admin or coach
@@ -22,7 +23,25 @@ export const getRooms = async (req, res) => { // room = coaching (DB model)
         found = await find({ col: "coaching", query: { userId: req.user.id }, filter: { userId: 1, room: 1, courseName: 1, _id: 0 } }) // [{userId, room},{...}...]
     }
 
-    res.json(await processRooms({ req, found }))
+    // ! mutateRoomInfo
+    async function mutateRoomInfo(foundInfoWithRoomToken) {
+        // ! rewrite for user: in each Room user sees: coach img, coach name
+        // coach sees: user img, user name
+        if (req.user.role === "user") {
+            const foundCoachInfoToDisplayInEachUserRoom = []
+            for (let i = 0; i < found.length; i++) {
+                foundCoachInfoToDisplayInEachUserRoom.push(await find({ col: "courses", query: { courseName: found[i]?.courseName }, filter: { coachName: 1, img: 1, _id: 0 } }))
+            }
+
+            foundInfoWithRoomToken.forEach((info, infoInd) => {
+                info.name = foundCoachInfoToDisplayInEachUserRoom?.[infoInd]?.[0]?.coachName
+                info.img = foundCoachInfoToDisplayInEachUserRoom?.[infoInd]?.[0]?.img
+            })
+        }
+        return foundInfoWithRoomToken
+    }
+
+    res.json(await processRooms({ req, found, mutateRoomInfo }))
 }
 
 // ! getRoomsSupport
@@ -41,7 +60,7 @@ export const getRoomsSupport = async (req, res) => {
 }
 
 // ! processRooms
-async function processRooms({ req, found }) {
+async function processRooms({ req, found, mutateRoomInfo }) {
     const foundUserIds = found.map(found => found?.userId) // [userId,userId,...]
     const foundRoomTokens = found.map(found => found.room) // [token,token,...]
 
@@ -50,13 +69,13 @@ async function processRooms({ req, found }) {
     for (let i = 0; i < foundUserIds.length; i++) {
         foundInfo.push(await find({ col: "users", query: { _id: foundUserIds[i] }, filter: { name: 1, img: 1, _id: 1 } }))
     }
-    
+
     let allMessages = await find({ col: "messages", query: { room: { $in: foundRoomTokens } }, filter: { msg: 1, room: 1, updatedAt: 1, userId: 1, isRead: 1, img: 1, _id: 0 } })
     const allMessagesReversed = [...allMessages].reverse()
     const lastMsgsArr = foundRoomTokens.map(token => allMessagesReversed.find(message => token === message.room && { msg: message.msg, room: message.room, createdAt: message.updatedAt, img: message.img }))
     const notReadMsgsArr = foundRoomTokens.map(token => allMessages.map(message => req.user.id !== message.userId && message.isRead === false && token === message.room).filter(leaveOnlyTrue => leaveOnlyTrue))
 
-    const foundInfoWithRoomToken = foundInfo.map((info, infoInd) => ({
+    let foundInfoWithRoomToken = foundInfo.map((info, infoInd) => ({
         name: info?.[0]?.name,
         img: info?.[0]?.img,
         userId: info?.[0]?._id,
@@ -69,18 +88,8 @@ async function processRooms({ req, found }) {
         courseName: found?.[infoInd]?.courseName
     }));
 
-    // ! rewrite for user: in each Room user sees: coach img, coach name
-    // coach sees: user img, user name
-    if (req.user.role === "user") {
-        const foundCoachInfoToDisplayInEachUserRoom = []
-        for (let i = 0; i < found.length; i++) {
-            foundCoachInfoToDisplayInEachUserRoom.push(await find({ col: "courses", query: { courseName: found[i]?.courseName }, filter: { coachName: 1, img: 1, _id: 0 } }))
-        }
-
-        foundInfoWithRoomToken.forEach((info, infoInd) => {
-            info.name = foundCoachInfoToDisplayInEachUserRoom?.[infoInd]?.[0]?.coachName
-            info.img = foundCoachInfoToDisplayInEachUserRoom?.[infoInd]?.[0]?.img
-        })
+    if (mutateRoomInfo) {
+        foundInfoWithRoomToken = await mutateRoomInfo(foundInfoWithRoomToken)
     }
 
     // clear mongo info

f4d9f1d6df051d6cb1ae9c331a4d5660f83db36e
enotowitch <en.enotowitch@gmail.com>
2024-02-12 10:31:34 +0200

    M: redefineUserRole: +new support

--- utils/redefineUserRole.js
+++ utils/redefineUserRole.js
@@ -2,9 +2,10 @@ import { findOne } from "../controllers/functions.js"
 
 export default async function redefineUserRole({ userEmail, userRole }) {
 
-    // redefine user role if admin has assigned new coach
+    // redefine user role if admin has assigned new coach or support
     const foundUpdatedByAdminAnyMomentCoachRole = await findOne({ col: "courses", query: { coachEmail: userEmail } })
-    const newRole = foundUpdatedByAdminAnyMomentCoachRole && userRole !== "admin" ? "coach" : userRole
+    let newRole = foundUpdatedByAdminAnyMomentCoachRole && userRole !== "admin" ? "coach" : userRole
+    newRole = userEmail === process.env.SUPPORT_EMAIL || userEmail === process.env.SUPPORT_EMAIL2 ? "support" : newRole
 
     return newRole
 }

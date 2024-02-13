efea9c177470432e951b88f00012461108577eb4
enotowitch <en.enotowitch@gmail.com>
2024-02-13 12:21:33 +0200

    !F: setUserRole: CURRENTLY support role is defined in redefineUserRole

--- utils/setUserRole.js
+++ utils/setUserRole.js
@@ -1,11 +1,12 @@
 export default function setUserRole(userEmail) {
     if (userEmail === process.env.ADMIN_EMAIL || userEmail === process.env.ADMIN_EMAIL2) {
         return "admin"
-    } 
-    if (userEmail === process.env.SUPPORT_EMAIL || userEmail === process.env.SUPPORT_EMAIL2) {
-        return "support"
-    } 
-    if(userEmail !== process.env.ADMIN_EMAIL || userEmail !== process.env.ADMIN_EMAIL2 || userEmail !== process.env.SUPPORT_EMAIL || userEmail !== process.env.SUPPORT_EMAIL2) {
+    }
+    // CURRENTLY support role is defined in redefineUserRole
+    // if (userEmail === process.env.SUPPORT_EMAIL || userEmail === process.env.SUPPORT_EMAIL2) {
+    //     return "support"
+    // } 
+    if (userEmail !== process.env.ADMIN_EMAIL || userEmail !== process.env.ADMIN_EMAIL2 || userEmail !== process.env.SUPPORT_EMAIL || userEmail !== process.env.SUPPORT_EMAIL2) {
         return "user"
     }
 }
